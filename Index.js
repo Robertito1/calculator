d1f0d03dcb2560dfa8957c51024a3edc2ae601b0
Robert Orazu <orazurobert@yahoo.com>
2021-03-09 08:56:15 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,4 +1,4 @@
-class Queue {
+class Calculator {
    constructor() {
      }
    add(num1,num2) {
@@ -8,7 +8,36 @@ class Queue {
      }
      return num1 + num2
    }
+
+   subtract(num1,num2) {
+     if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+       console.log('please use arguments of type number')
+       return
+     }
+     return num1 - num2
+   }
+
+   multiply(num1,num2) {
+     if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+       console.log('please use arguments of type number')
+       return
+     }
+     return num1 * num2
+   }
+
+   divide(num1,num2) {
+     if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+       console.log('please use arguments of type number')
+       return
+     }
+     return num1 / num2
+   }
   } 
+
+//To test this program, initialize a new calculator using the code below
+// let myCalculator = new Calculator
+// let five = myCalculator.add(2,3)
+// console.log(five) //5
 
  
    
